16c21635b0986f3f8feb94e7d9dd503413f767c8
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-03-07 14:16:15 -0500

    Adding classes to decompilation

--- spec/class_spec.rb
+++ spec/class_spec.rb
@@ -11,9 +11,23 @@ class A
   nil
 end
 RESULT
+
+    @nonempty_class = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
+class A
+  puts A
+end
+CLASS
+class A
+  puts(A)
+end
+RESULT
   end
   
   it "decompiles an empty class" do
     @empty_class.assert_correct
+  end
+  
+  it "decompiles a class with some executable code (but no defs)" do
+    @nonempty_class.assert_correct
   end
 end
