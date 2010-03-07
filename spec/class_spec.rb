33ebd8b5eb96ca15d1ceac5123bbb57a2d4309ce
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-03-07 14:18:50 -0500

    Specs for modules, classes, singleton class

--- spec/class_spec.rb
+++ spec/class_spec.rb
@@ -14,11 +14,31 @@ RESULT
 
     @nonempty_class = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
 class A
-  puts A
+  attr_accessor :hello
 end
 CLASS
 class A
-  puts(A)
+  attr_accessor(:hello)
+end
+RESULT
+
+    @simple_module = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
+module A
+  module_function :silly
+end
+CLASS
+module A
+  module_function(:silly)
+end
+RESULT
+
+    @simple_singleton_class = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
+class << self
+  x = self
+end
+CLASS
+class << self
+  x = self
 end
 RESULT
   end
@@ -29,5 +49,13 @@ RESULT
   
   it "decompiles a class with some executable code (but no defs)" do
     @nonempty_class.assert_correct
+  end
+  
+  it "decompiles a simple module" do
+    @simple_module.assert_correct
+  end
+  
+  it "decompiles access to singleton classes" do
+    @simple_singleton_class.assert_correct
   end
 end
