f777c83c1b72a00dcf0144e1988e195321e92b8c
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-03-11 14:13:54 -0500

    Basic branching is coming out nicely

--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -57,17 +57,53 @@ def test(x)
   end
 end
 RESULT
+
+    @trailing_if = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
+def test(x)
+  x = 10 if y
+end
+CLASS
+def test(x)
+  if (y)
+    x = 10
+  else
+    nil
+  end
+end
+RESULT
+
+    @trailing_unless = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
+def test(x)
+  x = 10 unless y
+end
+CLASS
+def test(x)
+  if (y)
+    nil
+  else
+    x = 10
+  end
+end
+RESULT
   end
   
   it "can decompile a single if statement" do
     @single_if.assert_correct_ignoring_indentation
   end
   
-  it "can decompile elsif branches (albeit in an ugly manner)" do
+  it "can decompile elsif branches retaining the structure" do
     @elsif_branches.assert_correct_ignoring_indentation
   end
   
-  it "can decompile a simple unless statement" do
+  it "can decompile a simple unless statement to equivalent code" do
     @single_unless.assert_correct_ignoring_indentation
+  end
+  
+  it "can decompile a guard-if statement to equivalent code" do
+    @trailing_if.assert_correct_ignoring_indentation
+  end
+  
+  it "can decompile a guard-unless statement to equivalent code" do
+    @trailing_unless.assert_correct_ignoring_indentation
   end
 end
