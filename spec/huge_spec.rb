1495208cd42bbef4fb29b7e0a6a630beffe60147
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-03-10 22:31:15 -0500

    Added difficult spec to pass

---
+++ spec/huge_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Really Large Decompilations" do
+  before do
+    @complex_nesting = CompiledDecompilationTestCase.new <<CLASS, <<RESULT
+module Hello
+  include SomeModule
+  class World < Universe
+    attr_accessor :name
+    self.attr_writer :moons
+    def initialize(some_thing, *args)
+      super
+      @name = some_thing.to_s
+    end
+    
+    def orbit!(&blk)
+      @moons.each do |moon|
+        moon.rotate
+        moon.revolve! :twice
+        yield
+      end
+    end
+  end
+end
+CLASS
+module Hello
+  include(SomeModule)
+  class World < Universe
+    attr_accessor(:name)
+    self.attr_writer(:moons)
+    def initialize(some_thing, *args)
+      super
+      @name = some_thing.to_s
+    end
+    def orbit!(&blk)
+      @moons.each do |moon|
+        moon.rotate
+        moon.revolve!(:twice)
+        yield
+      end
+    end
+  end
+end
+RESULT
+  end
+  
+  it "decompiles a complex nesting of modules, classes, definitions, and blocks" do
+    @complex_nesting.assert_correct
+  end
+end
