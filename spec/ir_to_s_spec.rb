7b8c3fa1808c931ad5db89c65c1a234b09d9dbeb
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-03-29 17:47:40 -0400

    more IR speccing.

--- spec/ir_to_s_spec.rb
+++ spec/ir_to_s_spec.rb
@@ -6,6 +6,10 @@ describe "Intermediate Representation Strinfication" do
     r(:lit, 5).to_s.should.equal "5"
   end
 
+  it "converts literal strings" do
+    r(:lit, "hello").to_s.should.equal "\"hello\""
+  end
+
   it "converts local getvar expressions" do
     r(:getvar, "somevar").to_s.should.equal "somevar"
   end
@@ -33,5 +37,21 @@ describe "Intermediate Representation Strinfication" do
   it "converts array literal expressions" do
     r(:array, [r(:lit, 3), r(:lit, :hello)]).to_s.should.equal "[3, :hello]"
   end
-  
+
+  it "converts inclusive range literal expressions" do
+    r(:range, r(:lit, "aaa"), r(:lit, "zzz"), true).to_s.should.equal "(\"aaa\"..\"zzz\")"
+  end
+
+  it "converts exclusive range literal expressions" do
+    r(:range, r(:lit, 3), r(:lit, 300), false).to_s.should.equal "(3...300)"
+  end
+
+  it "converts a simple infix expression" do
+    r(:infix, :+, [r(:lit, 3), r(:lit, 4)]).to_s.should.equal "3 + 4"
+  end
+
+  it "converts an infix expression with many arguments" do
+    r(:infix, :*, [r(:lit, "hi"), r(:lit, 3), r(:lit, 20)]).to_s.should.equal "\"hi\" * 3 * 20"
+  end
+
 end
